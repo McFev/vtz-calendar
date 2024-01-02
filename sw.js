8dcca2a22ffa253978c917047ab3c8905ed014fc
McFev <mcfev@mail.ru>
2024-01-02 10:48:19 +0300

    1.0.9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v1_0_8';
-const dynamicCacheName = 'dynamic-cache-v1_0_8';
+const staticCacheName = 'static-cache-v1_0_9';
+const dynamicCacheName = 'dynamic-cache-v1_0_9';
 
 const staticAssets = [
     './',
