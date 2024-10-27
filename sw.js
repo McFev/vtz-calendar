149181feaf7126343d630085ceab2a5910a48aba
McFev <mcfev@mail.ru>
2024-10-27 21:16:07 +0300

    format + no zoom

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v1_0_12';
-const dynamicCacheName = 'dynamic-cache-v1_0_12';
+const staticCacheName = 'static-cache-v1_0_13';
+const dynamicCacheName = 'dynamic-cache-v1_0_13';
 
 const staticAssets = [
     './',
