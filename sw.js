3c4f1410c701ad4201215c6db06a863a177f5793
McFev <mcfev@mail.ru>
2025-12-01 18:18:01 +0300

    add ariaLabel

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v1_1_0';
-const dynamicCacheName = 'dynamic-cache-v1_1_0';
+const staticCacheName = 'static-cache-v1_1_1';
+const dynamicCacheName = 'dynamic-cache-v1_1_1';
 
 const staticAssets = [
     './',
