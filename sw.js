6b2e74b71a0d07593d3a938d2bc1e79702b7537d
McFev <mcfev@mail.ru>
2025-12-10 12:21:18 +0300

    Yandex.Metrika counter 3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v1_1_3';
-const dynamicCacheName = 'dynamic-cache-v1_1_3';
+const staticCacheName = 'static-cache-v1_1_4';
+const dynamicCacheName = 'dynamic-cache-v1_1_4';
 
 const staticAssets = [
     './',
