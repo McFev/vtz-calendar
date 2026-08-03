63f01dc1b378d9bdd9f283a31bdd0c04863568d9
McFev <mcfev@mail.ru>
2026-08-03 09:25:18 +0300

    html - color-scheme: light dark (correct)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v1_1_9';
-const dynamicCacheName = 'dynamic-cache-v1_1_9';
+const staticCacheName = 'static-cache-v1_1_10';
+const dynamicCacheName = 'dynamic-cache-v1_1_10';
 
 const staticAssets = [
     './',
